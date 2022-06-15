862ae3c6d3a335380a86db3bb0f7f76196bea5f6
ShlomoRuckban <Shlomoruckban@gmail.com>
2022-06-15 19:53:57 +0300

    added cartId to user API

--- store-app/src/components/index.js
+++ store-app/src/components/index.js
@@ -2,5 +2,6 @@ import Footer from "./footer/footer";
 import NavBar from "./nav-bar/nav-bar";
 import Category from "./category/category";
 import Item from "./item/item";
+import CartItem from "./cart-item/cart-item"
 
-export { Footer, NavBar, Category, Item };
+export { Footer, NavBar, Category, Item, CartItem };
